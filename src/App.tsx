bc8ba838c2c080aa0eefc7b7a14317c11647429f
kiran <spkiran.suresh@gmail.com>
2023-08-03 12:43:51 +0530

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Text fontSize={"2xl"}>New challenge</Text>
-      <Button size="sm" variant="outline" colorScheme="messenger">
-        Button
-      </Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main" "footer"`, // base means mobile devices, we need only single column
+          lg: `"nav nav" "sideNav main" "footer footer"`, // lg means large devices(>= 1024px) , 2 column by default
+        }}
+      >
+        <GridItem area={"nav"} bg="red">
+          Nav
+        </GridItem>
+        <Show above={"lg"}>
+          <GridItem area={"sideNav"} bg="gold">
+            Side nav
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg={"silver"} color={"#FFF"}>
+          Main
+        </GridItem>
+        <GridItem area={"footer"} bg="red.400">
+          Footer
+        </GridItem>
+      </Grid>
     </>
   );
 }
