6d00fe65036e1d84a341cef4b4ba47cdae087ed2
kiran <spkiran.suresh@gmail.com>
2023-08-04 17:07:05 +0530

    defining the column width

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,17 @@ function App() {
           base: `"nav" "main" "footer"`, // base means mobile devices, we need only single column
           lg: `"nav nav" "sideNav main" "footer footer"`, // lg means large devices(>= 1024px) , 2 column by default
         }}
+        templateColumns={{
+          // assiging width for our columns
+          base: "1fr", // in base, we render one column, so strech to full width using 1fr(1 fraction)
+          lg: "200px 1fr", // in lg, first column: nav will be 200px and second column: main will take the rest
+        }}
       >
         <GridItem area={"nav"} bg="#151515">
           <NavBar />
         </GridItem>
         <Show above={"lg"}>
-          <GridItem area={"sideNav"}>
+          <GridItem area={"sideNav"} paddingX={2}>
             <GenreList />
           </GridItem>
         </Show>
