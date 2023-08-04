cba98a1e0b9978906d5e19ef3e4e3965fb2af908
kiran <spkiran.suresh@gmail.com>
2023-08-04 17:12:39 +0530

    spiner for genre loading

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,14 @@
-import { List, Text } from "@chakra-ui/react";
+import { List, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import GenreItem from "./GenreItem";
 
 function GenreList() {
-  const { data: genres, error } = useGenres();
+  const { data: genres, error, isLoading } = useGenres();
+  if (isLoading) return <Spinner />;
   return (
     <>
       {error && <Text>{error}</Text>}
+
       <List>
         {genres?.map((genre) => (
           <GenreItem genre={genre} />
