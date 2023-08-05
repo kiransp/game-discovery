3cc8e7e098693314b237d447d717adfea9dd4e77
kiran <spkiran.suresh@gmail.com>
2023-08-05 17:21:51 +0530

    handling games without image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const cropImageUrl = (imageUrl: string) => {
   const target = "/media/";
-  if (imageUrl === null) return imageUrl;
+  if (!imageUrl) return noImage;
   const index = imageUrl.indexOf(target) + target.length;
   return imageUrl.slice(0, index) + "crop/600/400/" + imageUrl.slice(index);
 };
